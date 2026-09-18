918ad6beec5669b6f495ef33220ca7afd686fef9
Vibe <vibe@local>
2026-09-18 09:54:35 +0000

    Fix Cloth Config API: AutoConfig.getConfigScreen -> AutoConfigClient.getConfigScreen
    Co-authored-by: damienmillet <damienmillet@users.noreply.github.com>

--- src/client/java/net/diprosalik/mcmistral/client/modmenu/ModMenuIntegration.java
+++ src/client/java/net/diprosalik/mcmistral/client/modmenu/ModMenuIntegration.java
@@ -2,12 +2,12 @@ package net.diprosalik.mcmistral.client.modmenu;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 import net.diprosalik.mcmistral.mistral.MistralConfig;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(MistralConfig.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(MistralConfig.class, parent).get();
     }
 }
